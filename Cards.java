a0c0aafd66617d773229a09c271d6bb7be2704e1
shraddha <kshraddhagupta@gmail.com>
2016-07-07 15:02:08 +0530

    card class updated with compare method

--- Cards.java
+++ Cards.java
@@ -1,7 +1,49 @@
 public class Cards {
+    private int type, value;
+    private String[] suit = {"Clubs",  "Diamonds","Hearts", "Spades"};
+    private String[] cardValue = {"","Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
 
-	
-	
+     Cards(int values, int types) {
+        this.type = types; 
+        this.value = values;
+    }
+     
+    public String display() {
+        String finalCard = cardValue[value] + " of " + suit[type];
 
+        return finalCard;
+    }
+    
+    public static String compare(Cards a,Cards b ){
+    	String greatest=" ";
+    	if(a.value > b.value){
+    		greatest = a.display();
+    	}else if(a.value<b.value){
+    	greatest = b.display();
+    	}else if(a.value == b.value){
+    		if(a.type >b.type){
+    			greatest = a.display();
+    		}else
+    			greatest = b.display();	
+    	}
+    	return greatest;   	
+    }
+    public static void main(String[] args)
+    {
+        Cards c1= new Cards(6,2);
+        Cards c2 = new Cards(6,1);
+        System.out.println(c1.compare(c1,c2));
+        
+
+
+    }
+    
+    
+    
+    
 }
+
+
+
+
 
