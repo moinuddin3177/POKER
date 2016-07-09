75f1c25ead5757c2d693c58816b4cd380b8d6809
Narsing <kommetan@gmail.com>
2016-07-09 09:38:05 +0530

    modified cardclass

--- Card.java
+++ Card.java
@@ -1,12 +1,11 @@
-
 import java.util.*;
 
 public class Card {
-	public int suit, pip;
-	private String[] suits = {"clubs",  "diamonds","hearts", "spades"};
-	private String[] cardValue = {"","Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
-
-
+	int suit;
+	int pip;
+	private String[] suits = { "clubs", "diamonds", "hearts", "spades" };
+	private String[] cardValue = { "", "Ace", "2", "3", "4", "5", "6", "7",
+			"8", "9", "10", "Jack", "Queen", "King" };
 
 	Card(int values, String type) {
 		this.pip = values;
@@ -20,20 +19,9 @@ public class Card {
 		return finalCard;
 	}
 
-	public String compare(Card a,Card b ){
-		String greatest=" ";
-		if(a.pip > b.pip){
-			greatest = "c1";
-		}else if(a.pip<b.pip){
-			greatest = "c2";
-		}else if(a.pip == b.pip){
-			if(a.suit >b.suit){
-				greatest = "c1";
-			}else
-				greatest = "c2";	
-		}
-		return greatest;   	
-	}
+	public int compare(Card c) {
+		return pip - c.pip;
 
+	}
 
 }
